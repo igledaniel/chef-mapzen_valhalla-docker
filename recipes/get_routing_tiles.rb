afc4d2b803f0f83ac1648dbc2a4f04837f5749bc
Grant Heffernan <heffergm@gmail.com>
2016-09-14 09:53:39 -0400

    region from stack data bag

--- recipes/get_routing_tiles.rb
+++ recipes/get_routing_tiles.rb
@@ -7,13 +7,15 @@
 # All rights reserved - Do Not Redistribute
 #
 
+stack = search('aws_opsworks_stack').first
+
 # go get the tiles
 execute 'pull tiles' do
   user  node[:valhalla][:user][:name]
   cwd   node[:valhalla][:base_dir]
   command <<-EOH
     echo -n https://s3.amazonaws.com/#{node[:valhalla][:s3bucket]}/#{node[:valhalla][:s3bucket_dir]}/ > latest_tiles.txt &&
-    aws --region us-east-1 s3 ls s3://#{node[:valhalla][:s3bucket]}/#{node[:valhalla][:s3bucket_dir]}/ | grep -F tiles_ | awk '{print $4}' | sort | tail -n 1 >> latest_tiles.txt
+    aws --region #{stack['region']} s3 ls s3://#{node[:valhalla][:s3bucket]}/#{node[:valhalla][:s3bucket_dir]}/ | grep -F tiles_ | awk '{print $4}' | sort | tail -n 1 >> latest_tiles.txt
   EOH
   timeout 3600
 end
