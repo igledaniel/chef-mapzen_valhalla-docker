8600b6e59dfd35bd4e488b55fe6d3c2abce26618
Grant Heffernan <heffergm@gmail.com>
2016-09-14 08:48:29 -0400

    tiles as user

--- recipes/get_routing_tiles.rb
+++ recipes/get_routing_tiles.rb
@@ -9,7 +9,8 @@
 
 # go get the tiles
 execute 'pull tiles' do
-  cwd node[:valhalla][:base_dir]
+  user  node[:valhalla][:user][:name]
+  cwd   node[:valhalla][:base_dir]
   command <<-EOH
     echo -n https://s3.amazonaws.com/#{node[:valhalla][:s3bucket]}/#{node[:valhalla][:s3bucket_dir]}/ > latest_tiles.txt &&
     aws --region us-east-1 s3 ls s3://#{node[:valhalla][:s3bucket]}/#{node[:valhalla][:s3bucket_dir]}/ | grep -F tiles_ | awk '{print $4}' | sort | tail -n 1 >> latest_tiles.txt
@@ -18,7 +19,8 @@ end
 
 # open them up
 execute 'extract tiles' do
-  cwd node[:valhalla][:base_dir]
+  user  node[:valhalla][:user][:name]
+  cwd   node[:valhalla][:base_dir]
   command <<-EOH
     rm -rf tmp_tiles old_tiles &&
     mkdir tmp_tiles &&
@@ -29,6 +31,7 @@ end
 
 # move them into place
 execute 'move tiles' do
-  cwd node[:valhalla][:base_dir]
+  user  node[:valhalla][:user][:name]
+  cwd   node[:valhalla][:base_dir]
   command "mv #{node[:valhalla][:tile_dir]} old_tiles; mv tmp_tiles #{node[:valhalla][:tile_dir]}"
 end
