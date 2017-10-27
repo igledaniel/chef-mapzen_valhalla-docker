b63321b6ec07c3047d85b04dfde85e6eafa548f3
Grant Heffernan <heffergm@gmail.com>
2017-10-27 09:00:04 -0400

    tiles

--- recipes/get_routing_tiles.rb
+++ recipes/get_routing_tiles.rb
@@ -15,7 +15,7 @@ execute 'pull tiles' do
   cwd   node[:valhalla][:base_dir]
   command <<-EOH
     aws --region #{stack['region']} s3 ls s3://#{node[:valhalla][:s3bucket]}/#{node[:valhalla][:s3bucket_dir]}/ | grep -F planet_ | awk '{print $4}' | sort | tail -n 1 > latest_tiles.txt &&
-    aws --region #{stack['region']} s3 cp s3://#{node[:valhalla][:s3bucket]}/#{node[:valhalla][:s3bucket_dir]}/$(cat latest_tiles.txt) tiles.tar
+    aws --region #{stack['region']} s3 cp s3://#{node[:valhalla][:s3bucket]}/#{node[:valhalla][:s3bucket_dir]}/$(cat latest_tiles.txt) #{node[:valhalla][:tile_dir]}/tiles.tar
   EOH
   timeout 3600
 end
