244634ea01d916025713e61a50d159acb5c6e2b4
Grant Heffernan <heffergm@gmail.com>
2017-10-27 08:53:03 -0400

    fix

--- recipes/get_routing_tiles.rb
+++ recipes/get_routing_tiles.rb
@@ -14,7 +14,6 @@ execute 'pull tiles' do
   user  node[:valhalla][:user][:name]
   cwd   node[:valhalla][:base_dir]
   command <<-EOH
-    echo -n https://s3.amazonaws.com/#{node[:valhalla][:s3bucket]}/#{node[:valhalla][:s3bucket_dir]}/ > latest_tiles.txt &&
     aws --region #{stack['region']} s3 ls s3://#{node[:valhalla][:s3bucket]}/#{node[:valhalla][:s3bucket_dir]}/ | grep -F planet_ | awk '{print $4}' | sort | tail -n 1 >> latest_tiles.txt &&
     aws --region #{stack['region']} s3 cp s3://#{node[:valhalla][:s3bucket]}/#{node[:valhalla][:s3bucket_dir]}/$(cat latest_tiles.txt) tiles.tar
   EOH
